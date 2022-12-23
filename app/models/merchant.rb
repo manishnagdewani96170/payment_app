39d2b8fd6288f029f1d252aa84b4e69f88cdef27
Manish N <nagdewanimanis@gmail.com>
2022-12-23 23:43:29 +0530

    Merchant view with bootstrap added

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,13 @@
 class Merchant < ApplicationRecord
   
+  # Constants
+  STATUSES = %w(active inactive)
+
   # Validations
   validates :name, :email, presence: true
   validates :email, uniqueness: true
   validates :total_transaction_sum, numericality: { greater_than_or_equal_to: 0 }
-  validates :status, inclusion: { in: %w(active inactive), message: "%{value} is not a valid status" }
+  validates :status, inclusion: { in: STATUSES, message: "%{value} is not a valid status" }
 
   # Scope
   scope :active, -> { where(status: 'active') }
